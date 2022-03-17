e73548bf1bd54ff4204fc8dd6f6e2669c6460536
pushpa32 <101447532+pushpa32@users.noreply.github.com>
2022-03-17 14:34:58 +0530

    fresh commit

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
-const express = require('express');
-const port = process.env.port || 8000;
-const app = express();
+import express from 'express'
+import { connection } from './connection.js'
+import { demo } from './router/demo.js'
+import { rol } from './router/rol.js'
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const app = express()
+const PORT = 3000
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// CONNECTION EVENTS
+// When successfully connected
+connection.on('connected', () => {
+    app.listen(PORT, () => {
+        console.log("Server started: localhost:3000")
+    })
 });
 
-app.listen(port, () => { console.log('Our App Is Up And Running!'); });
-module.exports = app
+// If the connection throws an error
+connection.on('error', (err) => {
+    console.log('Mongoose default connection error: ' + err)
+});
+
+// When the connection is disconnected
+connection.on('disconnected', () => {
+    console.log('Mongoose default connection disconnected')
+    process.exit(0)
+});
+
+// If the Node process ends, close the Mongoose connection 
+process.on('SIGINT', () => {
+    connection.close(() => {
+        console.log('Mongoose connection disconnected through app termination')
+        process.exit(0)
+    });
+});
+
+
+/*
+    employee login and register
+    ROL -> Register or Login
+*/
+app.use('/api/rol', rol)
+
+//demo: for testing purpose only
+app.use('/demo', demo)
+
+/*
+handle 404 path not found
+*/
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+})
